e557b19bbe03f6fe90f5634be18d6648f0e2beb3
wickwirez-beep <wickwirez-beep@users.noreply.github.com>
2026-08-09 21:55:28 -0500

    fix: add missing getValue import for State delegate

--- app/src/main/java/com/wickwirez/linkwarden/ScanningIndicator.kt
+++ app/src/main/java/com/wickwirez/linkwarden/ScanningIndicator.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
